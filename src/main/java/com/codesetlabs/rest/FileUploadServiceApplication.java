0c7f042c02594bba35fb0410003f5842da3439ab
Sameera Wickramasekara <itssamwiks@gmail.com>
2016-05-07 19:46:37 +0530

    initial commit

---
+++ src/main/java/com/codesetlabs/rest/FileUploadServiceApplication.java
@@ -0,0 +1,12 @@
+package com.codesetlabs.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadServiceApplication.class, args);
+	}
+}
